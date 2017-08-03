ed0064f6749ef9db9f6b195416455a14776d8e7f
salusn <salu.s@lightrains.com>
2017-08-03 13:56:17 +0530

    Added form for calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import { form } from 'semantic-ui-react';
+import { Form,Header } from 'semantic-ui-react';
 
 
 class Calculator extends Component {
 	render() {
 		return (
 			<div>
-			Calculator
+				<Header as='h2'>Ethereum Mining Profitability Calculator</Header>
+				<Form>
+					<Header as='h3'>Calculator</Header>
+    				<Form.Field>
+      					<label>Enter hashrate</label>
+      					<input placeholder='Your hashrate in MH per s' />
+    				</Form.Field>
+
+   	 				<Form.Field>
+      					<label>Network hashrate</label>
+      					<input placeholder='66207.56678163535' />
+    				</Form.Field>
+
+				    <Form.Field>
+      					<label>Blocktime</label>
+      					<input placeholder='22.625' />
+    				</Form.Field>
+
+    				<Form.Field>
+      					<label>1 ETH price</label>
+      					<input placeholder='221.53' />
+   	 				</Form.Field>
+  				</Form>
 			</div>
 		)
 	}
