056029676cd275e9c3cbe0c293a5263c50ccbd47
salusn <salu.s@lightrains.com>
2017-08-03 16:55:28 +0530

    Added form values to the state

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { Form,Header,Table,Label } from 'semantic-ui-react';
 
-
+var ethereumStats = {"blockTime":11.90625,"difficulty":58538976908480,"priceUsd":10.0876,"lastupdate":1467959742};
+var nethash = (ethereumStats.difficulty / ethereumStats.blockTime) / 1e9;
 class Calculator extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      hashrate: '',
+      networkhashrate : nethash,
+      blockTime : ethereumStats.blockTime,
+      ethPrice : ethereumStats.priceUsd
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
+  handleChange(event) {
+    this.setState({hashrate: event.target.value});
+  }
+
 
 	render() {
 		return (
@@ -16,22 +29,22 @@ class Calculator extends Component {
 					<Header as='h3'>Calculator</Header>
     				<Form.Field>
       					<label>Enter hashrate</label>
-      					<input placeholder='Your hashrate in MH per s' />
+      					<input placeholder='Your hashrate in MH per s' value={this.state.hashrate} onChange={this.handleChange}/>
     				</Form.Field>
 
    	 				<Form.Field>
       					<label>Network hashrate</label>
-      					<input placeholder='66207.56678163535' />
+      					<input placeholder='Network hashrate' value = {this.state.networkhashrate} readOnly/>
     				</Form.Field>
 
 				    <Form.Field>
       					<label>Blocktime</label>
-      					<input placeholder='22.625' />
+      					<input placeholder='Block time' value={this.state.blockTime} readOnly/>
     				</Form.Field>
 
     				<Form.Field>
       					<label>1 ETH price</label>
-      					<input placeholder='221.53' />
+      					<input placeholder='ETH Price' value={this.state.ethPrice} readOnly />
    	 				</Form.Field>
   				</Form>
 
